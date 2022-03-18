136010b4d8f038c72e22a9522cf2403e602ab4f2
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-03-18 15:59:36 +0000

    task3-2

--- task3.js
+++ task3.js
@@ -1,6 +1,12 @@
 var mcount = 0;
 function countMouse() {
     mcount++;
-    document.getElementById("mcount").innerHTML=mcount;
-    
+    document.getElementById("mcount").innerHTML = mcount;
+}
+
+function post() {
+    const d =  new Date();
+    document.getElementsByClassName("post-time")[0].innerHTML = d;
+    var p = document.getElementById("text-input").value;
+    document.getElementsByClassName("post-content")[0].innerHTML = p;
 }
